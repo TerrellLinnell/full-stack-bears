8afbed6503d9ded6c6f368fe0307be8b42b0e038
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 11:44:21 -0700

    added a parameter to the api endpoint

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/api/greeting',function(req,res){
-  res.send("Happy Monday");
+app.get('/api/greeting/:name', function(req, res){
+  var name = req.params.name;
+  res.send("Happy Monday " + name);
 });
 
 app.get('/api/number', function(req, res){
@@ -12,6 +13,12 @@ app.get('/api/number', function(req, res){
 app.get('/api/numbers', function(req, res){
   res.send("32, 14, 76, 5");
 });
+
+
+
+
+
+
 
 app.listen(3000, function(){
   console.log('Express server up and running on port 3000');
