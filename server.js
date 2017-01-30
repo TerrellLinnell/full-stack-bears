40e8580d165437ee3eb6ecfceed3fe1606af4670
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 10:52:07 -0700

    used node http module to spin up simple server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.send("Hello world!!!");
+}).listen(3000);
+
+console.log("Server is up and running!!");
