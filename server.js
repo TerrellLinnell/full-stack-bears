e157042fef06288fd2209cb0bfd209780017db74
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 14:19:37 -0700

    added a models directory and put bears.js in it. bears.js became a new mogoose schema.

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var app = express();
+var Bear = require('./models/bear');
+
 var mongoose = require('mongoose');
-
 mongoose.connect("mongodb://localhost/fullstack-bears");
-
-app.get('/api/greeting/:name', function(req, res){
-  var name = req.params.name;
-  res.send("Happy Monday " + name);
-});
-
-app.get('/api/number', function(req, res){
-  res.send("32");
-});
-
-app.get('/api/numbers', function(req, res){
-  res.send("32, 14, 76, 5");
-});
 
 app.get('/api/bears', function(req, res){
   res.send({ message:"You found the bears route!! 😁😁😁😁😁😁"});
 });
-
 
 
 
