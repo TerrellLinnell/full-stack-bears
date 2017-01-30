7946ee8f205de09a30b0ffcf9ad2bbd303c174e8
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 11:32:07 -0700

    added 2 more api endpoints

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get('/api/greeting',function(req,res){
   res.send("Happy Monday");
 });
 
+app.get('/api/number', function(req, res){
+  res.send("32");
+});
+
+app.get('/api/numbers', function(req, res){
+  res.send("32, 14, 76, 5");
+});
+
 app.listen(3000, function(){
   console.log('Express server up and running on port 3000');
 });
