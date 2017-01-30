1515fc051f5b1a55a3d73ae0aee52431d3e0f550
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 15:53:39 -0700

    Created api route for updating a bear's information

--- server.js
+++ server.js
@@ -51,7 +51,32 @@ app.get('/api/bears/:bear_id', function(req, res){
       res.json(bearData);
     }
   })
-})
+});
+
+app.put('/api/bears/:bear_id', function(req, res){
+  Bear.findById(req.params.bear_id, function(err, bearData){
+    if(err){
+      console.log("Error finding one specific bear");
+    }
+    else{
+      bearData.name = req.body.name ? req.body.name :bearData.name;
+      bearData.species = req.body.species ? req.body.species :bearData.species;
+      bearData.age = req.body.age ? req.body.age :bearData.age;
+      bearData.weight = req.body.weight ? req.body.weight :bearData.weight;
+      bearData.location = req.body.location ? req.body.location :bearData.location;
+      bearData.attitude = req.body.attitude ? req.body.attitude :bearData.attitude;
+
+      bearData.save(function(e, updatedBear){
+        if (e) {
+          console.log("Error with Bearer");
+        }
+        else{
+          res.json(updatedBear);
+        }
+      })
+    }
+  })
+});
 
 
 app.listen(3000, function(){
