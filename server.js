e4d612c4293586d97eed7cc403144a1920d72f34
terrell linnell <terrell.linnell@gmail.com>
2017-01-30 14:49:34 -0700

    implemented POST resource for bears resource

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var app = express();
 var Bear = require('./models/bear');
+var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/fullstack-bears");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/api/bears', function(req, res){
   res.send({ message:"You found the bears route!! 😁😁😁😁😁😁"});
 });
 
+app.post('/api/bears', function(req, res){
+  var bear = new Bear({
+    age:req.body.age,
+    species:req.body.species,
+    name:req.body.name,
+    weight:req.body.weight,
+    location:req.body.location,
+    attitude:req.body.attitude
 
+  });
+
+  bear.save(function(err, bearData) {
+    if (err) {
+      consoel.log(err, "Error with your bearer");
+    }
+    else{
+      res.json(bearData);
+    }
+  });
+})
 
 
 app.listen(3000, function(){
