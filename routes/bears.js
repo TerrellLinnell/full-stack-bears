5912f9dc0a19c9fabf3c036c39bba528bc90bdd6
terrell linnell <terrell.linnell@gmail.com>
2017-02-01 12:12:56 -0700

    created routes directory and tranferred routes from server.js to index.js and bears.js within routes directory

---
+++ routes/bears.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var Bear = require('../models/bear');
+var moment = require('moment');
+
+
+var router = new express.Router();
+
+router.use(function(req, res, next) {
+  console.log("Running the Express router.");
+  next();
+})
+
+router.route('/bears')
+  .get(function(req, res){
+    Bear.find(function(err, data) {
+      if(err){
+        console.log("Error finding all bearers");
+      }
+      else{
+        res.json(data);
+      }
+    });
+  })
+.post(function(req, res){
+  var bear = new Bear({
+    age:req.body.age,
+    species:req.body.species,
+    name:req.body.name,
+    weight:req.body.weight,
+    location:req.body.location,
+    attitude:req.body.attitude
+  });
+  bear.save(function(err, bearData) {
+    if (err) {
+      console.log(err, "Error with your bearer");
+    }
+    else{
+      res.json(bearData);
+    }
+  });
+});
+
+router.route('/bears/:bear_id')
+  .get (function(req, res){
+    Bear.findById(req.params.bear_id, function(err, bearData){
+      if(err){
+        console.log("Error finding one specific bear");
+      }
+      else{
+        res.json(bearData);
+      }
+    });
+  })
+
+  .put(function(req, res){
+    Bear.findById(req.params.bear_id, function(err, bearData){
+      if(err){
+        console.log("Error finding one specific bear");
+      }
+      else{
+        bearData.name = req.body.name ? req.body.name :bearData.name;
+        bearData.species = req.body.species ? req.body.species :bearData.species;
+        bearData.age = req.body.age ? req.body.age :bearData.age;
+        bearData.weight = req.body.weight ? req.body.weight :bearData.weight;
+        bearData.location = req.body.location ? req.body.location :bearData.location;
+        bearData.attitude = req.body.attitude ? req.body.attitude :bearData.attitude;
+
+        bearData.save(function(e, updatedBear){
+          if (e) {
+            console.log("Error with Bearer");
+          }
+          else{
+            res.json(updatedBear);
+          }
+        });
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+    Bear.remove({ _id: req.params.bear_id}, function(err, b){
+      if (err) {
+        console.log(err, "COULD NOT DELETE BEAR!!!");
+      }
+      else{
+        res.json({ message: "BEAR DELETED 😫"})
+      }
+    });
+  });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,11 +14,14 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  var date = moment();
-  res.render('index', {name: "Stranger", date: date.format("MMM Do YYYY"),
-                       time: date.format("hh:mm:ss A")});
-});
+var indexRoute = require('./routes/index.js');
+var bearsRoute = require('./routes/bears.js');
+
+// app.get('/', function(req, res) {
+//   var date = moment();
+//   res.render('index', {name: "Stranger", date: date.format("MMM Do YYYY"),
+//                        time: date.format("hh:mm:ss A")});
+// });
 
 app.get('/bears', function(req, res) {
   Bear.find(function(err, data) {
@@ -31,84 +34,88 @@ app.get('/bears', function(req, res) {
   })
 });
 
-app.get('/api/bears', function(req, res){
-  Bear.find(function(err, data) {
-    if(err){
-      console.log("Error finding all bearers");
-    }
-    else{
-      res.json(data);
-    }
-  })
-});
+// app.get('/api/bears', function(req, res){
+//   Bear.find(function(err, data) {
+//     if(err){
+//       console.log("Error finding all bearers");
+//     }
+//     else{
+//       res.json(data);
+//     }
+//   })
+// });
+//
+// app.post('/api/bears', function(req, res){
+//   var bear = new Bear({
+//     age:req.body.age,
+//     species:req.body.species,
+//     name:req.body.name,
+//     weight:req.body.weight,
+//     location:req.body.location,
+//     attitude:req.body.attitude
+//
+//   });
 
-app.post('/api/bears', function(req, res){
-  var bear = new Bear({
-    age:req.body.age,
-    species:req.body.species,
-    name:req.body.name,
-    weight:req.body.weight,
-    location:req.body.location,
-    attitude:req.body.attitude
+//    bear.save(function(err, bearData) {
+//     if (err) {
+//       console.log(err, "Error with your bearer");
+//     }
+//     else{
+//       res.json(bearData);
+//     }
+//   });
+// })
 
-  });
+// app.get('/api/bears/:bear_id', function(req, res){
+//   Bear.findById(req.params.bear_id, function(err, bearData){
+//     if(err){
+//       console.log("Error finding one specific bear");
+//     }
+//     else{
+//       res.json(bearData);
+//     }
+//   })
+// });
 
-   bear.save(function(err, bearData) {
-    if (err) {
-      console.log(err, "Error with your bearer");
-    }
-    else{
-      res.json(bearData);
-    }
-  });
-})
+// app.put('/api/bears/:bear_id', function(req, res){
+//   Bear.findById(req.params.bear_id, function(err, bearData){
+//     if(err){
+//       console.log("Error finding one specific bear");
+//     }
+//     else{
+//       bearData.name = req.body.name ? req.body.name :bearData.name;
+//       bearData.species = req.body.species ? req.body.species :bearData.species;
+//       bearData.age = req.body.age ? req.body.age :bearData.age;
+//       bearData.weight = req.body.weight ? req.body.weight :bearData.weight;
+//       bearData.location = req.body.location ? req.body.location :bearData.location;
+//       bearData.attitude = req.body.attitude ? req.body.attitude :bearData.attitude;
+//
+//       bearData.save(function(e, updatedBear){
+//         if (e) {
+//           console.log("Error with Bearer");
+//         }
+//         else{
+//           res.json(updatedBear);
+//         }
+//       })
+//     }
+//   })
+// });
 
-app.get('/api/bears/:bear_id', function(req, res){
-  Bear.findById(req.params.bear_id, function(err, bearData){
-    if(err){
-      console.log("Error finding one specific bear");
-    }
-    else{
-      res.json(bearData);
-    }
-  })
-});
+// app.delete('/api/bears/:bear_id', function(req, res) {
+//   Bear.remove({ _id: req.params.bear_id}, function(err, b){
+//     if (err) {
+//       console.log(err, "COULD NOT DELETE BEAR!!!");
+//     }
+//     else{
+//       res.json({ message: "BEAR DELETED 😫"})
+//     }
+//   })
+// });
 
-app.put('/api/bears/:bear_id', function(req, res){
-  Bear.findById(req.params.bear_id, function(err, bearData){
-    if(err){
-      console.log("Error finding one specific bear");
-    }
-    else{
-      bearData.name = req.body.name ? req.body.name :bearData.name;
-      bearData.species = req.body.species ? req.body.species :bearData.species;
-      bearData.age = req.body.age ? req.body.age :bearData.age;
-      bearData.weight = req.body.weight ? req.body.weight :bearData.weight;
-      bearData.location = req.body.location ? req.body.location :bearData.location;
-      bearData.attitude = req.body.attitude ? req.body.attitude :bearData.attitude;
+app.use('/', indexRoute);
+app.use('/api', bearsRoute);
 
-      bearData.save(function(e, updatedBear){
-        if (e) {
-          console.log("Error with Bearer");
-        }
-        else{
-          res.json(updatedBear);
-        }
-      })
-    }
-  })
-});
-
-app.delete('/api/bears/:bear_id', function(req, res) {
-  Bear.remove({ _id: req.params.bear_id}, function(err, b){
-    if (err) {
-      console.log(err, "COULD NOT DELETE BEAR!!!");
-    }
-    else{
-      res.json({ message: "BEAR DELETED 😫"})
-    }
-  })
-});
 
 app.listen(3000, function(){
   console.log('Express server up and running on port 3000');
