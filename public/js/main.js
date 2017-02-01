3bc827e96a3070de94df2d26b500ee1584b6ca47
terrell linnell <terrell.linnell@gmail.com>
2017-02-01 11:04:50 -0700

    created all CRUD ui  can now delete add and update the bears with the click of a button

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   console.log("We are linked to the static js file");
+  var updatingBear;
   $('.deleteBear').on('click', function(){
-    var bearId = $(this).attr('id');
+    var bearId = $(this).attr('id').slice(4);
     console.log(bearId);
     $.ajax({
       url: '/api/bears/'+bearId,
@@ -38,8 +39,36 @@ $('#newBear').submit(function (e) {
 });
 
 $('.updateBear').on('click', function () {
-  var bearId = $(this).attr('id').slice(7)
+  var bearId = $(this).attr('id').slice(7);
+  updatingBear = bearId;
   console.log(bearId);
-})
+  $.get('/api/bears/'+bearId).done(function(data){
+    console.log(data);
+    $('#name').val(data.name);
+    $('#species').val(data.species);
+    $('#age').val(data.age);
+    $('#weight').val(data.weight);
+    $('#location').val(data.location);
+    $('#attitude').val(data.attitude);
+  });
+});
 
+$('#updateBearButton').on('click', function(e) {
+  e.preventDefault();
+  var updateData = {name: $('#name').val(),
+                    age: $('#age').val(),
+                    location: $('#location').val(),
+                    weight: $('#weight').val(),
+                    species: $('#species').val(),
+                    attitude: $('#attitude').val(),
+                  };
+      $.ajax({
+        url: '/api/bears/' + updatingBear,
+        method: 'PUT',
+        data: updateData
+      }).done(function(data) {
+        console.log(data)
+        window.location = '/bears';
+      })
+  });
 });
